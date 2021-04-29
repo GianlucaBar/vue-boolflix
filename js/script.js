9fdb21046c7cd1444eb6168d124a2f60326a7d37
GianlucaBar <gianlucabarce@gmail.com>
2021-04-29 14:41:26 +0200

    milestone 1 completed - raw

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var app = new Vue(
+    {
+        el: '#root',
+
+        data: {
+            albums: [],
+            inputSearch: '',
+            movieCards: []
+        },
+
+        methods: {
+            getSearchResult(){
+                axios
+                .get('https://api.themoviedb.org/3/search/movie?api_key=7848f97dd1bd380d77cb8f9495749dba', {
+                    params: {
+                        query: this.inputSearch,
+                        page: 1
+                    }
+                })
+                .then( (response) => {
+                    const result = response.data;
+                    
+
+                    this.movieCards = result.results;
+                    console.log(result.results)
+                });
+            }
+		},
+
+        // mounted() {
+        //         
+        // },
+
+    });
