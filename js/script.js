3c33a53415dee186b798c61af689ce225d9b5e18
GianlucaBar <gianlucabarce@gmail.com>
2021-04-30 16:36:10 +0200

    stuck

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ var app = new Vue(
         el: '#root',
 
         data: {
-            inputSearch: '',
+            inputSearch: 'marvel',
             movieCards: [],
             tvShowCards: [],
-            discoverMovies: [],
-            discoverTvShows: []
+            // discoverMovies: [],
+            // discoverTvShows: []
+            thisCast: []
         },
 
         methods: {
@@ -24,8 +25,9 @@ var app = new Vue(
                 })
                 .then( (response) => {
                     const result = response.data;
-                    
                     this.movieCards = result.results;
+
+                    
                 });
 
                 axios
@@ -47,35 +49,76 @@ var app = new Vue(
             getVoteOnFive(vote){
                 const voteOnFive = Math.ceil(vote / 2)
                 return voteOnFive;
+            },
+
+            getCast(currentObj, id){
+                if( this.movieCards.includes(currentObj) ){
+                    axios
+                    .get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=e05661b069389f9a2788162b272f96a8`, {
+                        params: {
+                        
+                        }
+                    })
+                    .then( (response) => {
+                        const castNames = [];
+                        let cast = response.data.cast;
+                        
+                        cast = cast.slice(0, 5);
+                        cast.forEach(element => {
+                            castNames.push(element.name)
+                        });
+                        console.log(castNames);
+                        return castNames;
+                    });
+                } else{
+                    axios
+                    .get(`https://api.themoviedb.org/3/tv/${id}/credits?api_key=e05661b069389f9a2788162b272f96a8`, {
+                        params: {
+                        
+                        }
+                    })
+                    .then( (response) => {
+                        const castNames = [];
+                        let cast = response.data.cast;
+                        
+                        cast = cast.slice(0, 5);
+                        cast.forEach(element => {
+                            castNames.push(element.name)
+                        });
+                        console.log(castNames);
+                        return castNames;
+                    });
+
+                }
             }
 
 		},
 
         // preacarico la pagina con una chiamata api discover 
         mounted() {
-            axios
-            .get('https://api.themoviedb.org/3/discover/tv?api_key=7848f97dd1bd380d77cb8f9495749dba', {
-                params: {
+            // axios
+            // .get('https://api.themoviedb.org/3/discover/tv?api_key=7848f97dd1bd380d77cb8f9495749dba', {
+            //     params: {
         
-                }
-            })
-            .then( (response) => {
-                const result = response.data;
+            //     }
+            // })
+            // .then( (response) => {
+            //     const result = response.data;
                 
-                this.discoverTvShows = result.results;
-            });
+            //     this.discoverTvShows = result.results;
+            // });
             
-            axios
-                .get('https://api.themoviedb.org/3/discover/movie?api_key=7848f97dd1bd380d77cb8f9495749dba', {
-                    params: {
+            // axios
+            //     .get('https://api.themoviedb.org/3/discover/movie?api_key=7848f97dd1bd380d77cb8f9495749dba', {
+            //         params: {
     
-                    }
-                })
-                .then( (response) => {
-                    const result = response.data;
+            //         }
+            //     })
+            //     .then( (response) => {
+            //         const result = response.data;
                     
-                    this.discoverMovies = result.results;
-                });
+            //         this.discoverMovies = result.results;
+            //     });
         },
 
     });
